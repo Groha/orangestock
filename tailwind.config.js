8dcd9f7f89a288e343edd6b2e2286d6593b55422
Groha <groholskaya.kris@gmail.com>
2021-11-01 22:09:43 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    content: [`./public/**/*.html`]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
